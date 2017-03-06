87ace8ce5c7bf8422fae7f88595e6a591152325a
turncoordinator <robo86@gmail.com>
2017-03-06 13:03:06 -0500

    Add files via upload

---
+++ categories.py
@@ -0,0 +1,201 @@
+exclude_category =["Bicycle > Accessories >Brands", \
+"Bicycle > Accessories > Electronics > Video Cameras", \
+"Bicycle > Accessories > Electronics > Cameras", \
+"Bicycle > Accessories > Trailers/Strollers > Cargo Trailers", \
+"Bicycle > Accessories > Trailers/Strollers > Child Trailers", \
+"Bicycle > Accessories > Trailers/Strollers > Strollers/Joggers", \
+"Bicycle > Accessories > Trailers/Strollers > Trailer Bikes", \
+"Bicycle > Accessories > Trailers/Strollers > Pet Bicycle Carriers", \
+"Bicycle > Accessories > Media/Resources > Software", \
+"Bicycle > Car Racks > Roof Mount Cargo Boxes/ Carriers", \
+"Bicycle > Fitness Equipment > Elliptical Machines", \
+"Bicycle > Fitness Equipment > Indoor Cycles", \
+"Bicycle > Fitness Equipment > Inversion Products", \
+"Bicycle > Fitness Equipment > Recumbent Exercise Bikes", \
+"Bicycle > Fitness Equipment > Rowers", \
+"Bicycle > Fitness Equipment > Treadmills", \
+"Bicycle > Fitness Equipment > Upright Exercise Bikes", \
+"Bicycle > Fitness Equipment > Steppers", \
+"Bicycle > Fitness Equipment > Vertical Knee Raise Machine", \
+"Bicycle > Fitness Equipment > Weights", \
+"Bicycle > Demo Products > Wheels", \
+"Bicycle > Novelties/Gift Ideas > Novelties/ Gift Ideas", \
+"Bicycle > Novelties/Gift Ideas > Holiday Gift Ideas", \
+"Bicycle >Gift Cards", \
+"Bicycle > Bikes > BMX > BMX", \
+"Bicycle > Bikes > BMX > Freestyle", \
+"Bicycle > Bikes > BMX > Freestyle | Bicycle > Bikes > BMX > Jump", \
+"Bicycle > Bikes > Children's > 12-Inch (2-4 yr. old)", \
+"Bicycle > Bikes > Children's > 12-Inch (2-4 yr. old) | Bicycle > Bikes > Children's > Running Bikes", \
+"Bicycle > Bikes > Children's > 16-Inch (3-6 yr. old)", \
+"Bicycle > Bikes > Children's > 16-Inch (3-6 yr. old) | Bicycle > Bikes > BMX > BMX", \
+"Bicycle > Bikes > Children's > 16-Inch (3-6 yr. old) | Bicycle > Bikes > BMX > Freestyle", \
+"Bicycle > Bikes > Children's > 18-Inch (4-7 yr. old)", \
+"Bicycle > Bikes > Children's > 18-Inch (4-7 yr. old) | Bicycle > Bikes > BMX > Freestyle", \
+"Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old)", \
+"Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old) | Bicycle > Accessories > Trailers/Strollers > Trailer Bikes", \
+"Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old) | Bicycle > Bikes > BMX > BMX", \
+"Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old) | Bicycle > Bikes > BMX > Freestyle", \
+"Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old) | Bicycle > Bikes > BMX > Jump", \
+"Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Children's > 24-Inch (7+ yr. old) | Bicycle > Bikes > BMX > Freestyle", \
+"Bicycle > Bikes > Children's > Running Bikes", \
+"Bicycle > Bikes > Children's > Scooters", \
+"Bicycle > Bikes > Children's > Tricycles", \
+"Bicycle > Bikes > Comfort", \
+"Bicycle > Bikes > Comfort | Bicycle > Bikes > Cruiser", \
+"Bicycle > Bikes > Comfort | Bicycle > Bikes > Cruiser | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Comfort | Bicycle > Bikes > Hybrid", \
+"Bicycle > Bikes > Comfort | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Commuter/Urban > Fixed/One-Speed", \
+"Bicycle > Bikes > Commuter/Urban > Fixed/One-Speed | Bicycle > Bikes > Cruiser", \
+"Bicycle > Bikes > Commuter/Urban > Fixed/One-Speed | Bicycle > Bikes > Hybrid | Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Commuter/Urban > Fixed/One-Speed | Bicycle > Bikes > Road > Track", \
+"Bicycle > Bikes > Commuter/Urban > Frames", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Comfort", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Comfort | Bicycle > Bikes > Cruiser", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Comfort | Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Comfort | Bicycle > Bikes > Hybrid", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Comfort | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Cruiser", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Cruiser | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Hybrid", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Hybrid | Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Hybrid | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Hybrid | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Other > Adult Tricycles", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Other > Electric | Bicycle > Bikes > Other > Adult Tricycles", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Other > Folding | Bicycle > Bikes > Folding", \
+"Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Cruiser", \
+"Bicycle > Bikes > Cruiser | Bicycle > Bikes > Children's > 16-Inch (3-6 yr. old)", \
+"Bicycle > Bikes > Cruiser | Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old)", \
+"Bicycle > Bikes > Cruiser | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Cruiser | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Cruiser | Bicycle > Bikes > Other > Tandem > Comfort/Hybrid", \
+"Bicycle > Bikes > Cyclocross > Cyclocross Bikes", \
+"Bicycle > Bikes > Cyclocross > Cyclocross Bikes | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Cyclocross > Cyclocross Bikes | Bicycle > Bikes > Commuter/Urban > Fixed/One-Speed | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Cyclocross > Cyclocross Bikes | Bicycle > Bikes > Commuter/Urban > Multi-Speed", \
+"Bicycle > Bikes > Cyclocross > Cyclocross Bikes | Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Cyclocross > Cyclocross Bikes | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Cyclocross > Cyclocross Frames", \
+"Bicycle > Bikes > Cyclocross > Cyclocross Frames | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Electric Self Balancing Scooters", \
+"Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Folding", \
+"Bicycle > Bikes > Hybrid", \
+"Bicycle > Bikes > Hybrid | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Hybrid | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old) | Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Hybrid | Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Hybrid | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Hybrid | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Mountain > 26-Inch Wheel", \
+"Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers)", \
+"Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Unsuspended", \
+"Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > Dirt Jump", \
+"Bicycle > Bikes > Mountain > Dirt Jump | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > Downhill/Freeride", \
+"Bicycle > Bikes > Mountain > Downhill/Freeride | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Downhill/Freeride | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > Downhill/Freeride | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Front-Suspension", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old)", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 26-Inch Wheel", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 27.5-Inch Plus Wheel", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers)", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > 29-Inch Plus Wheel", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 29-Inch Plus Wheel", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Mountain > Front-Suspension | Bicycle > Bikes > Other > Electric | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Full-Suspension", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > 27.5-Inch Plus Wheel", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers)", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > Downhill/Freeride | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 27.5-Inch Plus Wheel", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Full-Suspension | Bicycle > Bikes > Other > Electric | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Unsuspended", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Children's > 16-Inch (3-6 yr. old)", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old)", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Children's > 20-Inch (5-8 yr. old) | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old) | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Mountain > 26-Inch Wheel", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Cruiser", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Hybrid", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Mountain > 27.5-Inch Plus Wheel", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Mountain > 650B Wheel", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Commuter/Urban > Frames | Bicycle > Bikes > Mountain > 26-Inch Wheel", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Fat Bikes", \
+"Bicycle > Bikes > Mountain > Unsuspended | Bicycle > Bikes > Mountain > Mountain Frames | Bicycle > Bikes > Mountain > 27.5-Inch Plus Wheel", \
+"Bicycle > Bikes > Other > Adult Tricycles", \
+"Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Other > Electric | Bicycle > Bikes > Other > Adult Tricycles", \
+"Bicycle > Bikes > Other > Folding", \
+"Bicycle > Bikes > Other > Folding | Bicycle > Bikes > Folding", \
+"Bicycle > Bikes > Other > Stationary | Bicycle > Fitness Equipment > Indoor Cycles", \
+"Bicycle > Bikes > Other > Stationary | Bicycle > Fitness Equipment > Indoor Cycles | Bicycle > Fitness Equipment > Upright Exercise Bikes", \
+"Bicycle > Bikes > Other > Tandem > All-Terrain", \
+"Bicycle > Bikes > Other > Tandem > Comfort/Hybrid", \
+"Bicycle > Bikes > Other > Tandem > Road", \
+"Bicycle > Bikes > Other > Unicycles", \
+"Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Road Frames", \
+"Bicycle > Bikes > Road > Road Frames | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Road Frames | Bicycle > Bikes > Road > Track", \
+"Bicycle > Bikes > Road > Sport/Performance", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Children's > 24-Inch (7+ yr. old)", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Commuter/Urban > Fixed/One-Speed", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Commuter/Urban > Fixed/One-Speed | Bicycle > Bikes > Road > Track", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Hybrid", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Cyclocross > Cyclocross Bikes | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Other > Electric", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Road > Road Frames", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Road > Road Frames | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Sport/Performance | Bicycle > Bikes > Road > Triathlon/Time Trial", \
+"Bicycle > Bikes > Road > Touring", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Commuter/Urban > Multi-Speed", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Hybrid | Bicycle > Bikes > Road > Endurance/Gravel | Bicycle > Bikes > Fitness", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers)", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Mountain > 29-Inch Wheel (29ers) | Bicycle > Bikes > Mountain > Unsuspended", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Mountain > Mountain Frames", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Road > Road Frames", \
+"Bicycle > Bikes > Road > Touring | Bicycle > Bikes > Road > Road Frames | Bicycle > Bikes > Commuter/Urban > Multi-Speed | Bicycle > Bikes > Road > Endurance/Gravel", \
+"Bicycle > Bikes > Road > Track", \
+"Bicycle > Bikes > Road > Triathlon/Time Trial", \
+"Bicycle > Bikes > Road > Triathlon/Time Trial | Bicycle > Bikes > Road > Road Frames"] 
